84d8e013451386d8548f28c0e7d959d2264f66d2
Erwan LE PIERRES <55241151+Erwan33800@users.noreply.github.com>
2022-05-23 09:26:57 +0200

    Add files via upload

---
+++ test_eenov_js/README.md
@@ -0,0 +1,32 @@
+### Get started !
+
+- Installes ruby sur ton pc
+- Lances `ruby main.rb --loyalty_card_id=1 --user_id=5` pour tester l'algorithme ruby : `main.rb`
+
+### The challenge
+
+On a des données d'utilisateurs avec leurs cartes de fidélités associées :
+
+Les utilisateurs peuvent posséder des cartes de fidélité.
+Les utilisateurs peuvent gagner des récompenses (points) pour une carte donnée.
+
+Il y a déjà quelques données à ta disposition (voir `input.json`) et un programme (voir `main.rb`)
+
+Le problème est que... la commande `ruby main.rb --loyalty_card_id=1 --user_id=5` renvoie toujours la même valeur !!
+
+Ecris du code qui reproduit la sortie de la commande, mais qui fonctionne en réalité avec les données `input.json`.
+
+### Que faire ?
+
+- Réalises un code propre et structuré (avec nodejs).
+- Utilises les lib/packages que tu veux.
+- Tu peux créer plusieurs fichiers et dossiers si tu veux.
+- You can (and we would love it if you do) write test (but it's ok if you are not confident with it).
+- Tu peux réaliser des tests unitaires si tu penses pouvoir en réaliser sur ton code.
+- Ecris dans ce README les commandes à réaliser pour exécuter ton programme.
+
+
+Pour lancer le programme : 
+
+il faut se placer dans le dossier reponseErwan en faisant : cd reponseErwan
+et il faut lancer la solution en faisant node index.js 'user_id' 'loyalty_card_id'   --> inscrire les id sans les apostrophes
---
+++ test_eenov_js/reponseErwan/index.js
@@ -0,0 +1,83 @@
+const jsonData = require('../input.json');
+
+// j'attribu une const à chaque objet pour les manipuler plus facilement
+const loyaltyCards = jsonData.loyalty_cards;
+const rewards = jsonData.rewards;
+
+const myArgs = process.argv.slice(2); // sert à ne pas prendre en compte les 2 premières lignes de process.argv qui ne sont pas utiles pour nous
+
+const userId = parseInt(myArgs[0]);
+const LCId = parseInt(myArgs[1]);
+var points;
+var pointsByLCByUser;
+var pointsByLCid;
+var nameLC;
+var loyaltyCardsObject = {};
+
+rewards.forEach(e => {
+    if (e.user_id === userId){
+
+        // récupération des datas d'un user spécifique
+        var filteredUser = rewards.filter(obj => obj.user_id === userId);
+
+        // sommes des points de toutes ses LC
+        points = filteredUser.map(item => item.points).reduce((prev, curr) => prev + curr, 0);
+
+        // détermine les LC communes à un user
+        filteredUser.forEach(i => {
+            if(i.loyalty_card_id === i.loyalty_card_id) {
+                const filteredLoyaltyCardByUser = filteredUser.filter(obj => obj.loyalty_card_id === i.loyalty_card_id);
+                pointsByLCByUser = filteredLoyaltyCardByUser.map(item => item.points).reduce((prev, curr) => prev + curr, 0);
+                // console.log("nb de points du user " + userId + " de la Lc de " + i.loyalty_card_id + " : " + pointsByLCByUser)
+                if (i.loyalty_card_id === 1) {
+                    var nameLC = "Carrefour"
+                }
+                if (i.loyalty_card_id === 2) {
+                    var nameLC = "Leclerc"
+                }
+                if (i.loyalty_card_id === 3) {
+                    var nameLC = "Macdo"
+                }
+                if (i.loyalty_card_id === 4) {
+                    var nameLC = "Fnac"
+                }
+                if (i.loyalty_card_id === 5) {
+                    var nameLC = "Subway"
+                }
+
+                loyaltyCardsObject = {
+                    "id" : i.loyalty_card_id,
+                    "total_points" : pointsByLCByUser,
+                    "name" : nameLC
+    
+                }
+                console.log(loyaltyCardsObject)
+            }
+        })
+    }
+    const filteredLoyaltyCards = rewards.filter(obj => obj.loyalty_card_id === LCId);
+    pointsByLCid = filteredLoyaltyCards.map(item => item.points).reduce((prev, curr) => prev + curr, 0);
+})
+
+loyaltyCards.forEach(e => {
+    if(e.id === LCId ){
+        nameLC = e.name
+    }
+})
+
+const resultat = {
+    "user": {
+        "id" : userId,
+        "total_points": points,
+        "loyalty_cards": loyaltyCardsObject
+    },
+    "loyalty_card" : {
+        "id": LCId,
+        "name": nameLC,
+        "total_points": pointsByLCid
+    }
+
+}
+
+console.log('Vous avez choisi l\'utilisateur :  ' + userId + " ainsi que la carte de fidélité numéro : " + LCId);
+console.log(resultat);
